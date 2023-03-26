a9f0cfbdca5c570ef8d783d9146d15fc55726cf8
Tim Fielder <timfielderdev@gmail.com>
2023-03-26 17:12:21 -0600

    add item add functionality

--- itemCollectionApp/models.py
+++ itemCollectionApp/models.py
@@ -7,7 +7,7 @@ class ItemCollection(models.Model):
 class CollectionItem(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
-    itemCollection = models.ForeignKey(ItemCollection, on_delete=models.CASCADE)
+    itemCollection = models.ForeignKey(ItemCollection, related_name='items', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
